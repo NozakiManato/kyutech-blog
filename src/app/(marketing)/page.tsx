f091e270b8d0486f9d1138a2fe15798ec59569a9
NozakiManato <yeqiaidou07@gmail.com>
2025-03-14 13:47:33 +0900

    hrefの中身をconfigファイルで定義した

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,4 +1,5 @@
 import { buttonVariants } from "@/components/ui/button";
+import { siteConfig } from "@/config/site";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 
@@ -8,10 +9,12 @@ const IndexPage = () => {
       <section className="pt-6 md:pt-10 lg:py-32 pb-8 md:pb-12">
         <div className="container text-center flex flex-col items-center gap-4 max-w-4xl">
           <Link
-            href={"/"}
+            href={siteConfig.links.instagram}
             className="bg-muted px-4 py-1.5 rounded-2xl font-medium text-sm"
+            target="_blank"
+            rel="noreferrer"
           >
-            Xをフォローする
+            インスタをフォローする
           </Link>
           <h1 className="font-extrabold text-3xl sm:text-5xl md:text-6xl lg:text-7xl">
             LocaLabo
@@ -28,7 +31,7 @@ const IndexPage = () => {
               始める
             </Link>
             <Link
-              href={"https://github.com/NozakiManato"}
+              href={siteConfig.links.github}
               className={cn(buttonVariants({ size: "lg", variant: "outline" }))}
               target="_blank"
               rel="noreferrer"
---
+++ src/config/site.ts
@@ -0,0 +1,12 @@
+import { SiteConfig } from "@/types";
+
+export const siteConfig: SiteConfig = {
+  name: "Kyutech Blog",
+  description: "",
+  url: "http://localhost:3000",
+  ogImage: "",
+  links: {
+    instagram: "https://www.instagram.com/manatou9999/",
+    github: "https://github.com/NozakiManato",
+  },
+};
