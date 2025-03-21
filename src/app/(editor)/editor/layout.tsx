fff968dbe756d44ac236c9f94f9f70b2365f961d
NozakiManato <yeqiaidou07@gmail.com>
2025-03-21 14:41:39 +0900

    editorページの追加

---
+++ src/app/(editor)/editor/layout.tsx
@@ -0,0 +1,25 @@
+import DashboardNav from "@/components/dashboard/dashboard-nav";
+import Footer from "@/components/footer/footer";
+import Header from "@/components/header/header";
+import { dashboardConfig } from "@/config/dashboard";
+import { PageLayoutProps } from "@/types";
+import React from "react";
+
+const MarketingLayout = ({ children }: PageLayoutProps) => {
+  return (
+    <>
+      <Header items={dashboardConfig.mainNav} />
+      <div className="container grid flex-1 gap-12 md:grid-cols-[200px_1fr] pt-5">
+        <aside className="hidden md:flex w-52 flex-col">
+          <DashboardNav items={dashboardConfig.sidebarNav} />
+        </aside>
+        <main className="flex flex-col w-full flex-1 overflow-hidden">
+          {children}
+        </main>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default MarketingLayout;
