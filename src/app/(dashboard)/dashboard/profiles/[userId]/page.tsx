63a8c6b947ac924d299763a19c589fa0b380ed9c
NozakiManato <yeqiaidou07@gmail.com>
2025-04-09 14:32:13 +0900

    Userのプロフィール画面遷移問題解決

--- src/app/(dashboard)/dashboard/profiles/[userId]/page.tsx
+++ src/app/(dashboard)/dashboard/profiles/[userId]/page.tsx
@@ -1,24 +1,27 @@
 import ProfileCard from "@/components/dashboard/profiles/profile-card";
 import { getUserTechSkills } from "@/lib/actions";
 import { requireAuth } from "@/lib/auth";
-import { getUserProfile } from "@/lib/prisma/user";
-import { notFound } from "next/navigation";
+import { notFound, redirect } from "next/navigation";
 
 type Props = {
-  params: { userId: string };
+  params: Promise<{ userId: string }>;
 };
 
 export default async function Profile(props: Props) {
   const { userId: currentUserId } = await requireAuth();
-  const userId = props.params.userId;
-  const profile = await getUserProfile(userId);
+  const userId = (await props.params).userId;
+  const { profile } = await requireAuth();
 
   if (!profile) {
-    notFound();
+    if (currentUserId === userId) {
+      redirect("/additional-info");
+    } else {
+      notFound();
+    }
   }
 
   const skills = await getUserTechSkills(profile.id);
-  const isOwnProfile = currentUserId === userId;
+  const isOwnProfile = currentUserId === profile.userId;
 
   return (
     <>
